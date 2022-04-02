0d54f3118b13b1eac7d497378ee5a5e3d0a35b23
Evan Pratten <ewpratten@gmail.com>
2022-04-02 11:29:43 -0400

    Menu routing system

--- game/game_logic/src/scenes/main_menu.rs
+++ game/game_logic/src/scenes/main_menu.rs
@@ -1,13 +1,26 @@
 //! This scene encompasses the main menu system
 
 use nalgebra as na;
-use raylib::{prelude::*, ffi::{Texture, GetMouseX, GetMouseY, IsMouseButtonDown}};
+use raylib::{
+    ffi::{GetMouseX, GetMouseY, IsMouseButtonDown, Texture},
+    prelude::*,
+};
 
 use crate::{
     discord::{DiscordChannel, DiscordRpcSignal},
     global_resource_package::GlobalResources,
     project_constants::ProjectConstants,
 };
+
+#[derive(Debug, Clone)]
+pub enum MenuStateSignal {
+    StartGame,
+    QuitGame,
+    DoMainMenu,
+    DoOptions,
+    DoCredits,
+    DoLeaderboard,
+}
 
 #[derive(Debug)]
 pub struct MainMenu {
@@ -26,21 +39,17 @@ impl MainMenu {
         }
     }
 
-    /// Handler for each frame
-    pub async fn render_frame(
+    pub async fn render_main_menu_frame(
         &mut self,
         raylib: &mut RaylibHandle,
         rl_thread: &RaylibThread,
         discord: &DiscordChannel,
         global_resources: &GlobalResources,
         constants: &ProjectConstants,
-    ) -> bool {
+    ) -> MenuStateSignal {
         // Handle updating discord RPC
         if !self.has_updated_discord_rpc {
-            discord
-                .send(DiscordRpcSignal::EndGameTimer)
-                .await
-                .unwrap();
+            discord.send(DiscordRpcSignal::EndGameTimer).await.unwrap();
             discord
                 .send(DiscordRpcSignal::ChangeDetails {
                     details: "Looking at a menu".to_string(),
@@ -58,15 +67,13 @@ impl MainMenu {
         draw.clear_background(Color::WHITE);
 
         //I wanna see where mouseeee
-        unsafe{
+        unsafe {
             let mut mouseX = GetMouseX();
             let mut mouseY = GetMouseY();
 
             draw.draw_text((&mouseX.to_string()), 20, 5, 20, Color::BLACK);
             draw.draw_text((&mouseY.to_string()), 70, 5, 20, Color::BLACK);
         }
-        
-
 
         // TODO: Render stuff
         //Initial Option placeholder words in the main menu
@@ -80,41 +87,77 @@ impl MainMenu {
         //Unsafe block??
         unsafe {
             //First two are starting X and Y position, last two finishing X and Y. Made to resemble a box
-            if GetMouseX() >= 100 && GetMouseY() >= 193 && GetMouseX() <= 290 && GetMouseY() <= 216{
+            if GetMouseX() >= 100 && GetMouseY() >= 193 && GetMouseX() <= 290 && GetMouseY() <= 216
+            {
                 //Insides while make a lil shade for it to look cool
                 draw.draw_text("Start Game", 103, 191, 34, Color::GRAY);
                 draw.draw_text("Start Game", 100, 190, 34, Color::BLACK);
                 if IsMouseButtonDown(0) {
-                    return true;
+                    return MenuStateSignal::StartGame;
                 }
             }
 
-            if GetMouseX() >= 100 && GetMouseY() >= 250 && GetMouseX() <= 222 && GetMouseY() <= 275{
+            if GetMouseX() >= 100 && GetMouseY() >= 250 && GetMouseX() <= 222 && GetMouseY() <= 275
+            {
                 draw.draw_text("Options", 103, 251, 34, Color::GRAY);
                 draw.draw_text("Options", 100, 250, 34, Color::BLACK);
-
             }
 
-            if GetMouseX() >= 100 && GetMouseY() >= 410 && GetMouseX() <= 222 && GetMouseY() <= 437{
+            if GetMouseX() >= 100 && GetMouseY() >= 410 && GetMouseX() <= 222 && GetMouseY() <= 437
+            {
                 draw.draw_text("Credits", 103, 411, 34, Color::GRAY);
                 draw.draw_text("Credits", 100, 410, 34, Color::BLACK);
-
             }
-            if GetMouseX() >= 100 && GetMouseY() >= 470 && GetMouseX() <= 316 && GetMouseY() <= 496{
+            if GetMouseX() >= 100 && GetMouseY() >= 470 && GetMouseX() <= 316 && GetMouseY() <= 496
+            {
                 draw.draw_text("Leaderboard", 103, 471, 34, Color::GRAY);
                 draw.draw_text("Leaderboard", 100, 470, 34, Color::BLACK);
-
             }
-            if GetMouseX() >= 100 && GetMouseY() >= 550 && GetMouseX() <= 162 && GetMouseY() <= 575{
+            if GetMouseX() >= 100 && GetMouseY() >= 550 && GetMouseX() <= 162 && GetMouseY() <= 575
+            {
                 draw.draw_text("Exit", 103, 551, 34, Color::GRAY);
                 draw.draw_text("Exit", 100, 550, 34, Color::BLACK);
-
             }
-
         }
 
-        // Return true if you want the game to start. 
-        // Otherwise, keep returning false until the player clicks the start button
-        return false;
+        // Return MenuStateSignal::StartGame if you want the game to start.
+        // Otherwise, keep returning MenuStateSignal::DoMainMenu until the player clicks the start button
+        return MenuStateSignal::DoMainMenu;
+    }
+
+    pub async fn render_options_frame(
+        &mut self,
+        raylib: &mut RaylibHandle,
+        rl_thread: &RaylibThread,
+        discord: &DiscordChannel,
+        global_resources: &GlobalResources,
+        constants: &ProjectConstants,
+    ) -> MenuStateSignal {
+
+        return MenuStateSignal::DoOptions;
+    }
+
+    pub async fn render_credits_frame(
+        &mut self,
+        raylib: &mut RaylibHandle,
+        rl_thread: &RaylibThread,
+        discord: &DiscordChannel,
+        global_resources: &GlobalResources,
+        constants: &ProjectConstants,
+    ) -> MenuStateSignal {
+
+        return MenuStateSignal::DoCredits;
+    }
+
+    pub async fn render_leaderboard_frame(
+        &mut self,
+        raylib: &mut RaylibHandle,
+        rl_thread: &RaylibThread,
+        discord: &DiscordChannel,
+        global_resources: &GlobalResources,
+        constants: &ProjectConstants,
+    ) -> MenuStateSignal {
+
+        return MenuStateSignal::DoLeaderboard;
     }
 }
