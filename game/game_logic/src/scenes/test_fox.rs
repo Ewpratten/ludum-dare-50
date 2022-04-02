e0fa16cc8fdb54231cd78d6f85e8033e5df01085
Evan Pratten <ewpratten@gmail.com>
2022-04-02 14:39:12 -0400

    Map rendering!

--- game/game_logic/src/scenes/test_fox.rs
+++ game/game_logic/src/scenes/test_fox.rs
@@ -1,18 +1,20 @@
 //! This "scene" is used only for testing animation and resource loading
 //! It should be removed once the game is being worked on
 
-use raylib::prelude::*;
 use nalgebra as na;
+use raylib::prelude::*;
 
 use crate::{
-    discord::DiscordChannel, global_resource_package::GlobalResources,
+    discord::DiscordChannel,
+    global_resource_package::GlobalResources,
     rendering::utilities::{anim_texture::AnimatedTexture, map_render::MapRenderer},
 };
 
 #[derive(Debug)]
 pub struct TestFoxScene {
     fox_animation: AnimatedTexture,
-    world_map: MapRenderer
+    world_map: MapRenderer,
+    camera: Camera2D,
 }
 
 impl TestFoxScene {
@@ -22,9 +24,24 @@ impl TestFoxScene {
         let fox = AnimatedTexture::new(raylib_handle, thread, "chr", "testFox").unwrap();
 
         // Load the map
-        let map_renderer = MapRenderer::new("map_gameMap.tmx").unwrap();
+        let map_renderer = MapRenderer::new("map_gameMap.tmx", raylib_handle, thread).unwrap();
 
-        Self { fox_animation: fox, world_map: map_renderer }
+        // Create a camera
+        let camera = Camera2D {
+            target: Vector2 { x: 0.0, y: 0.0 },
+            offset: Vector2 {
+                x: raylib_handle.get_screen_width() as f32,
+                y: (raylib_handle.get_screen_height() as f32) * -0.5,
+            },
+            rotation: 0.0,
+            zoom: 1.0,
+        };
+
+        Self {
+            fox_animation: fox,
+            world_map: map_renderer,
+            camera,
+        }
     }
 
     /// Handler for each frame
@@ -50,5 +67,27 @@ impl TestFoxScene {
             None,
             None,
         );
+
+        // Allow the camera to be moved with wasd
+        if draw.is_key_down(KeyboardKey::KEY_W) {
+            self.camera.target.y -= 5.0;
+        }
+        if draw.is_key_down(KeyboardKey::KEY_S) {
+            self.camera.target.y += 5.0;
+        }
+        if draw.is_key_down(KeyboardKey::KEY_A) {
+            self.camera.target.x -= 5.0;
+        }
+        if draw.is_key_down(KeyboardKey::KEY_D) {
+            self.camera.target.x += 5.0;
+        }
+
+        {
+            // Begin camera mode
+            let mut ctx2d = draw.begin_mode2D(self.camera);
+
+            // Render the map
+            self.world_map.render_map(&mut ctx2d, &self.camera, true);
+        }
     }
 }
